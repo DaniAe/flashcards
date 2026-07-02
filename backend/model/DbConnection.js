d966eaf39f6443f29e2a4125f231085d769a99fa
DaniAe <dani.ae277@gmail.com>
2026-07-01 21:20:06 -0400

    mongoose connection logs

--- backend/model/DbConnection.js
+++ backend/model/DbConnection.js
@@ -1,18 +1,20 @@
 require('dotenv').config();
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-exports.connect = async function(where){
-    let uri = process.env.DB_URI;
-    if(where === 'test') uri = process.env.TESTDB_URI;
-    if(process.env.CI) uri = "mongodb://adm:secret@localhost:27017"; //CI test
+exports.connect = async function (where) {
+  let uri = process.env.DB_URI;
+  if (where === 'test') uri = process.env.TESTDB_URI;
+  if (process.env.CI) uri = 'mongodb://adm:secret@localhost:27017'; //CI test
 
-    try{
-        await mongoose.connect(uri);
-    }catch(error){
-        console.log(error);
-    }
-}
+  try {
+    console.log('Attempting MongoDB connection...');
+    await mongoose.connect(uri);
+    console.log('MongoDB connected successfully');
+  } catch (error) {
+    console.log('MongoDB connection error:', error);
+  }
+};
 
-exports.disconnect = async function(){
-    await mongoose.connection.close();
-}
+exports.disconnect = async function () {
+  await mongoose.connection.close();
+};
